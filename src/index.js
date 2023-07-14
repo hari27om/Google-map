5e7986171e1cb8d86e1fa41dad3b50a5aca060e1
HARI OM TRIPATHI <122279024+hari27om@users.noreply.github.com>
2023-07-14 13:30:14 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <>
+//     <App />
+//   </>
+// );
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
